68546fdfe004a3bf0f85125cbe19002eefc2b8ec
yansonglei <129086809+yansonglei@users.noreply.github.com>
2023-03-28 14:11:45 +0000

    first commit

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
